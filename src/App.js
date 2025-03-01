4a0103a4daaec8c5d234aaaea72324bb947738b7
Austin O. <153394284+Aobier106@users.noreply.github.com>
2025-02-28 20:22:27 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React Deployment!</h1>
+      <p>This is my performance assessment for deploying a React app.</p>
+    </div>
+  );
+}
+
+export default App;
